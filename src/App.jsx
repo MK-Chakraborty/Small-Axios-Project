7a7168389e4116afd738646c5557bcf12754193e
MK-Chakraborty <mkchakraborty5@gmail.com>
2026-01-22 01:37:13 +0600

    get all data axios

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import Header from "./components/Header";
 import AddPost from "./components/Posts/AddPost";
 import AllPosts from "./components/Posts/AllPosts";
 import EditPost from "./components/Posts/EditPost";
-import { UpdatePostContext } from "./contexts";
+import { PostsContext, UpdatePostContext } from "./contexts";
 
 function App() {
   const { updatablePost } = useContext(UpdatePostContext);
+  const { error } = useContext(PostsContext);
 
   return (
     <section className="max-w-7xl border border-[#3b3440] mx-auto text-white">
       <Header />
       <AllPosts />
       {updatablePost.updatable ? <EditPost /> : <AddPost />}
+      {error && (
+        <div className="bg-red-500 text-white text-base p-1 mx-2 my-7 border border-white">
+          An Error Occurrd: {error}
+        </div>
+      )}
     </section>
   );
 }
--- src/providers/PostProvider.jsx
+++ src/providers/PostProvider.jsx
@@ -1,10 +1,36 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { PostsContext } from "../contexts";
 
 export default function PostProvider({ children }) {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/posts`);
+        if (response && response.data) {
+          setPosts(response.data);
+        }
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          setError(
+            error.response?.data?.message ||
+              error.response?.statusText ||
+              error.message,
+          );
+        } else {
+          setError("Something went wrong");
+        }
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
-    <PostsContext.Provider value={{ posts, setPosts }}>
+    <PostsContext.Provider value={{ posts, setPosts, error }}>
       {children}
     </PostsContext.Provider>
   );
